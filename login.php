767931375c5c559b3b96266dadbd74ecf2dea603
antoine <coding@MacBook-Pro-de-antoine.local>
2025-02-24 13:56:26 +0100

    page de login

---
+++ login.php
@@ -0,0 +1,35 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>connexion</title>
+</head>
+<header>
+</header>
+
+<body>
+    <h1>Connexion</h1>
+    <form method="post">    
+            <section class="formulaire">
+        <div class="container"> 
+            <input type="email"
+            id="email"
+            name="email"
+            placeholder="Email" />
+        </div>
+        <div class=container>
+            <input type="password"
+            id="mdp"
+            name="mdp"
+            placeholder="Mot de passe"/>
+
+            
+        </div>
+        <div class="container">
+            <button type="submit" name="connexion">se connecter</button>
+        </div>
+
+        <section class="insciption"> 
+            <a href="register.php" >toujours pas connecté ?</a>
+        </section>
+        </form>
+</body>
