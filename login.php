656be976c4f874f8e7473a2d5fd4be6e35e94567
antoine <coding@MacBook-Pro-de-antoine.local>
2025-02-24 14:03:55 +0100

    login

--- login.php
+++ login.php
@@ -32,4 +32,27 @@
             <a href="register.php" >toujours pas connecté ?</a>
         </section>
         </form>
+
+
+        <?php
+        if (isset($_POST['connexion'])) {
+        $email = htmlspecialchars(trim($_POST['email']));
+        $mdp = htmlspecialchars($_POST['mdp']);
+        $req = $db->prepare('SELECT id,email,mot_de_passe FROM utilisateur WHERE email = :email');
+        $req->execute(array("email" => $_POST['email']));
+        $data = $req->fetch();
+        if (!empty($data)){
+            if ($data['mot_de_passe'] === $mdp){
+                $_SESSION['user_id'] = $data['id'];
+                header('location:connexion.php');
+                echo "c'est bon vous êtes connecté";
+            }else{
+                echo "mauvais mot de passe";
+            }
+        }else{
+            echo "le compte n'existe pas";
+        }
+
+    }
+    ?>
 </body>
