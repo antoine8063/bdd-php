460b684e2ad27be3dc7961eb7cf7d4d6bbc46e3d
antoine <coding@MacBook-Pro-de-antoine.local>
2025-02-25 13:54:40 +0100

    css de register

--- register.php
+++ register.php
@@ -1,31 +1,43 @@
-<?php
-session_start();
-
-// Affichage des messages d'erreur ou de succès
-if (isset($_SESSION['error'])) { 
-    echo '<div class="error">' . htmlspecialchars($_SESSION['error']) . '</div>'; 
-    unset($_SESSION['error']);
-} elseif (isset($_SESSION['success'])) {
-    echo '<div class="success">' . htmlspecialchars($_SESSION['success']) . '</div>'; 
-    unset($_SESSION['success']); 
-}
-
-?>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jeu de Mémoire</title>
+    <link rel="stylesheet" href="assets/css/register.css">
+</head>
 
 <body>
 
-    <section>
+    <div>
         <h1>REGISTRATION</h1> 
-
+        <?php
+            session_start();
+            if (isset($_SESSION['error'])) {
+                echo '<p class="error-message">' . htmlspecialchars($_SESSION['error']) . '</p>';
+                unset($_SESSION['error']);
+            } elseif (isset($_SESSION['messagelogin'])) {
+                echo '<p class="success-message">' . htmlspecialchars($_SESSION['messagelogin']) . '</p>';
+                unset($_SESSION['messagelogin']);
+            }
+        ?>
         <!-- Formulaire d'inscription -->
         <form method="POST" action="utils/userRegister.php" enctype="multipart/form-data">
-            <input type="email" name="email" placeholder="  Email" required id="em"/>
-            <input type="text" name="username" placeholder="  Username" required id="us"/>
-            <input type="password" name="password" placeholder="  Password" required id="pas"/>
-            <input type="password" name="confirm_password" placeholder="  Confirm password" required id="conn"/>
-            <input type="submit" name="submit" value="Register" id="res"/>
+            <section class = container>    
+                <input type="email" name="email" placeholder="  Email" required id="em"/>
+            </section>
+            <section class = container>
+                <input type="text" name="username" placeholder="  Username" required id="us"/>
+            </section>
+            <section class = container>
+                <input type="password" name="password" placeholder="  Password" required id="pas"/>
+            </section>
+            <section class = container>
+                <input type="password" name="confirm_password" placeholder="  Confirm password" required id="conn"/>
+            </section>
+            <section class = container>
+                <input type="submit" name="submit" value="Register" id="res"/>
+            </section>
         </form>
-    </section>
+    </div>
 
 </body>
 </html>
