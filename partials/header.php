d5318aba1a5cb7387e68f1236ea8cad446f8a6b3
Vivien <marcuz.vivien@gmail.com>
2025-02-26 11:24:11 +0100

    side bar qui marche

--- partials/header.php
+++ partials/header.php
@@ -7,19 +7,21 @@
 
     <header>
     <div class="logo">BETFACTORY</div>
-        <ul class="nav-links">
-            <li><a href="index.php">ACCUEIL</a></li>
-            <li><a href="profile.php">COMPTE</a></li>
-            <li><a href="#">À PROPOS</a></li>
-        </ul>
-        <a href="#" class="bet-now">BET NOW</a>
+    <!-- Nav Links doivent avoir la classe 'nav-links' -->
+    <ul class="nav-links" id="nav-links">
+        <li><a href="index.php">ACCUEIL</a></li>
+        <li><a href="profile.php">COMPTE</a></li>
+        <li><a href="#">À PROPOS</a></li>
+    </ul>
+    <a href="#" class="bet-now">BET NOW</a>
 
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" alt="menu hamburger" class="menu-hamburger">
-            <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/>
-        </svg>
+    <!-- Icône du menu hamburger avec la classe 'menu-hamburger' -->
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" alt="menu hamburger" class="menu-hamburger" id="menu-hamburger">
+        <path d="M0 96C0 78.3 14.3 64 32 64l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 128C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32l384 0c17.7 0 32 14.3 32 32s-14.3 32-32 32L32 288c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32L32 448c-17.7 0-32-14.3-32-32s14.3-32 32-32l384 0c17.7 0 32 14.3 32 32z"/>
+    </svg>
 
-        <script src="assets/js/script-burger.js"></script>
-
-    </header>
+    <!-- Lien vers ton fichier JS -->
+    <script src="assets/js/script-burger.js"></script>
+</header>
 
     
