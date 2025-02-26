59b6d3169ded0ed031e89bbbb16366efb0728f30
Kum0.plm <mollivier07@gmail.com>
2025-02-26 11:19:02 +0100

    page html, css, js de la page pari

---
+++ assets/js/bet.js
@@ -0,0 +1,12 @@
+function toggleDetails(id) {
+    const details = document.getElementById(id);
+    if (details.style.display === "none" || details.style.display === "") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
+
+function placeBet(choice) {
+    alert(`Vous avez parié sur : ${choice}`);
+}
