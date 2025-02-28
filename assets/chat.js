6cb1c241845f24050713eb688a2955c776c6409b
antoine <coding@MacBook-Pro-de-antoine.local>
2025-02-28 15:06:56 +0100

    pseudo a la place de id

--- assets/chat.js
+++ assets/chat.js
@@ -12,7 +12,7 @@ document.addEventListener("DOMContentLoaded", function () {
             .then(data => {
                 chatBox.innerHTML = "";
                 data.forEach(msg => {
-                    chatBox.innerHTML += `<p><strong>Utilisateur ${msg.user_id}:</strong> ${msg.message}</p>`;
+                    chatBox.innerHTML += `<p><strong>${msg.username}:</strong> ${msg.message}</p>`;
 
                 });
                 chatContainer.scrollTop = chatContainer.scrollHeight;//scroll vers le bas
--- utils/getmessage.php
+++ utils/getmessage.php
@@ -4,7 +4,7 @@ require 'database.php';
 $db = Database::getInstance();
 $conn = $db->getConnection();
 
-$message = $conn->prepare("SELECT message, user_id FROM chat ORDER BY id ASC");
+$message = $conn->prepare("SELECT chat.message, users.username FROM chat JOIN users ON chat.user_id = users.id ORDER BY chat.id ASC");
 $message->execute();
 $messages = $message->fetchAll(PDO::FETCH_ASSOC);
 
